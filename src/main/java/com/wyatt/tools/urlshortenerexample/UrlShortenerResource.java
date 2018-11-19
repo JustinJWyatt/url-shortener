9f9af7c2da07636c0ccb9ea21b7a62073dea2bd8
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-19 12:47:48 -0600

    fixed url decoding

--- src/main/java/com/wyatt/tools/urlshortenerexample/UrlShortenerResource.java
+++ src/main/java/com/wyatt/tools/urlshortenerexample/UrlShortenerResource.java
@@ -6,37 +6,32 @@ import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
-@RequestMapping("/rest/url")
 @RestController
 public class UrlShortenerResource {
 
     StringRedisTemplate template;
 
-    @GetMapping("/{id}")
-    public String getUrl(@PathVariable String id){
+    @PostMapping("/create")
+    public String create(@RequestBody String url) throws UnsupportedEncodingException {
 
-        String url = template.opsForValue().get(id);
+        UrlValidator validator = new UrlValidator(new String[] { "http", "https"}, UrlValidator.ALLOW_ALL_SCHEMES);
 
-        return url;
-    }
+        url = URLDecoder.decode(url, "UTF-8");
 
-    @PostMapping
-    public String create(@RequestBody String url){
+        url = url.substring(0, url.length() - 1);
 
-        UrlValidator validator = new UrlValidator(new String[] { "http", "https"});
-
-        if (validator.is    Valid(url)){
+        if (validator.isValid(url)){
 
             String id = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
-
-            template.opsForValue().set(id, url);
 
             return id;
         }
 
-        throw new RuntimeException("URL Invalid: " + url);
+        throw new RuntimeException(url);
     }
 
 }
