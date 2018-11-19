8336fe52aaf5f7f72a6a7f27d6e8ce2c07cb728d
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-19 08:26:02 -0600

    added dependencies and routes

---
+++ src/main/java/com/wyatt/tools/urlshortenerexample/UrlShortenerResource.java
@@ -0,0 +1,42 @@
+package com.wyatt.tools.urlshortenerexample;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+
+@RequestMapping("/rest/url")
+@RestController
+public class UrlShortenerResource {
+
+    StringRedisTemplate template;
+
+    @GetMapping("/{id}")
+    public String getUrl(@PathVariable String id){
+
+        String url = template.opsForValue().get(id);
+
+        return url;
+    }
+
+    @PostMapping
+    public String create(@RequestBody String url){
+
+        UrlValidator validator = new UrlValidator(new String[] { "http", "https"});
+
+        if (validator.isValid(url)){
+
+            String id = Hashing.murmur3_32().hashString(url, StandardCharsets.UTF_8).toString();
+
+            template.opsForValue().set(id, url);
+
+            return id;
+        }
+
+        throw new RuntimeException("URL Invalid: " + url);
+    }
+
+}
