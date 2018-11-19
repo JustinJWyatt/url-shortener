687db8c020b679e440410aa2017a033b16bb1880
Justin Wyatt <justinwyatt@Justins-MacBook-Pro.local>
2018-11-19 07:04:13 -0600

    init commit

---
+++ src/main/java/com/wyatt/tools/urlshortenerexample/UrlShortenerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.wyatt.tools.urlshortenerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerExampleApplication.class, args);
+	}
+}
